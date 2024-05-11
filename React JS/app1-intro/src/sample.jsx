90f664000ae15013a23a677e6aee013e682b519e
mnethaji838 <mnethaji838@gmail.com>
2024-05-11 12:11:32 +0530

    add

---
+++ React JS/app1-intro/src/sample.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import './sample.css'
+
+export const student = () => {
+  const [myname] = useState("nethaji")
+  return (
+    <div id="nethaji">
+      <h2>
+        welcome to {myname} component
+      </h2>
+    </div>
+  )
+}
