99b0de1ddd3d18ccb46473938223b689d634cba8
mnethaji838 <mnethaji838@gmail.com>
2024-05-13 15:45:04 +0530

    add

---
+++ React JS/app3-intro/src/Nesha.jsx
@@ -0,0 +1,6 @@
+function nesha(
+){
+    return <h1>Hi Nesha</h1>
+}
+
+export default nesha
--- React JS/app3-intro/src/index.js
+++ React JS/app3-intro/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Info from './Info';
+import Sample from './Sample';
+import Nesha from './Nesha'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Nesha/>
   </React.StrictMode>
 );
 
