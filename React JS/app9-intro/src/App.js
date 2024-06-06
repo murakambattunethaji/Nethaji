e8765732d1b23b120655596e7b885e860d3e6d42
mnethaji838 <mnethaji838@gmail.com>
2024-06-06 20:58:59 +0530

    add

--- React JS/app9-intro/src/App.js
+++ React JS/app9-intro/src/App.js
@@ -1,10 +1,12 @@
+import { Recipe_Book } from "./class_parent_chaild_components/Recipe_Book2";
 import { StudentEnrollmentSystem } from "./class_parent_chaild_components/StudentEnrollmentSystem1";
 
 
 function App() {
   return (
     <div>
-      <StudentEnrollmentSystem />
+      {/* <StudentEnrollmentSystem /> */}
+      <Recipe_Book/>
     </div>
   );
 }
--- React JS/app9-intro/src/class_parent_chaild_components/StudentEnrollmentSystem1.jsx
+++ React JS/app9-intro/src/class_parent_chaild_components/StudentEnrollmentSystem1.jsx
@@ -7,25 +7,58 @@ export class StudentEnrollmentSystem extends Component {
     state = {
         persons: [
             {
-                id: 1,
-                email: "zxcvbnmlkjhgfdswrtyuio",
-                contact: 1234567890,
+                id: 10,
+                email: "mnethaji838@gmail.com",
+                contact: 7075806584,
+                name: "nethaji",
+                course: "Bsc"
             },
 
             {
-                name:"nethaji"
-            }
+                id: 11,
+                email: "mnethaji838@gmail.com",
+                contact: 7075806584,
+                name: "nethaji",
+                course: "Bsc"
+            },
+
+
         ],
     };
 
+    addperson = () => {
+        var newstate = { ...this.state }
+        newstate.persons.push(
+            {
+                id: 12,
+                email: "mnethaji838@gmail.com",
+                contact: 7075806584,
+                name: "Nethaji",
+                course: "Bsc"
+            }
+        )
+        this.setState(newstate)
+    }
+
+    deleteperson = (i) => {
+        console.log("deleteperson colled !!!", i)
+        var newstate={...this.state}
+        newstate.persons.splice(i,1)
+        this.setState(newstate)
+
+    }
+
     render() {
-        return <div>
-            {this.state.persons.map((person) => {
-                return (
-                    <Person_Child newperson={person} />
-                )
-            })}
-        </div>
+        return (
+            <div>
+                <button onClick={this.addperson}>add person</button>
+                {this.state.persons.map((person, i) => {
+                    return (
+                        <Person_Child newperson={person} key={i} handle={this.deleteperson} index={i} />
+                    )
+                })}
+            </div>
+        )
     }
 }
 
