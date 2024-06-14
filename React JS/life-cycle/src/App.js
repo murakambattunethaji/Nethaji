5811dd71b6b198f2b42e023f47f4a61bcabf31f0
mnethaji838 <mnethaji838@gmail.com>
2024-06-14 14:46:20 +0530

    add

--- React JS/life-cycle/src/App.js
+++ React JS/life-cycle/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Parent_component } from './Parent_component';
+import { Users } from './Function_Components/Users';
 
 function App() {
   return (
     <div>
-     <Parent_component/>
+     {/* <Parent_component/> */}
+     <Users/>
     </div>
   );
 }
