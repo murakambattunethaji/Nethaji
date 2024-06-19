6cd4a1b6b91015000e1d62bc80d2a584c08395fa
mnethaji838 <mnethaji838@gmail.com>
2024-06-19 11:55:47 +0530

    add

---
+++ React JS/Bootstrape/app10-intro/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './Bootstrape-Component/Users';
+
+function App() {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default App;
