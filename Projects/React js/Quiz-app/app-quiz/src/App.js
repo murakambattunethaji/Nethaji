5439ddc490b306b5d062b68f53724342f18d28e3
mnethaji838 <mnethaji838@gmail.com>
2024-08-15 11:45:43 +0530

    add

---
+++ Projects/React js/Quiz-app/app-quiz/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quiz from './quiz';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Quiz App</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/React js/Quiz-app/app-quiz/src/quiz.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import App from './App.css'
+
+
+const questions = [
+  {
+    questionText: 'What is the capital of France?',
+    answerOptions: [
+      { answerText: 'Berlin', isCorrect: false },
+      { answerText: 'Madrid', isCorrect: false },
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'Lisbon', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who is the CEO of Tesla?',
+    answerOptions: [
+      { answerText: 'Jeff Bezos', isCorrect: false },
+      { answerText: 'Elon Musk', isCorrect: true },
+      { answerText: 'Bill Gates', isCorrect: false },
+      { answerText: 'Tony Stark', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'The iPhone was created by which company?',
+    answerOptions: [
+      { answerText: 'Apple', isCorrect: true },
+      { answerText: 'Intel', isCorrect: false },
+      { answerText: 'Amazon', isCorrect: false },
+      { answerText: 'Microsoft', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'How many Harry Potter books are there?',
+    answerOptions: [
+      { answerText: '6', isCorrect: false },
+      { answerText: '7', isCorrect: true },
+      { answerText: '8', isCorrect: false },
+      { answerText: '5', isCorrect: false },
+    ],
+  },
+];
+
+function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="quiz">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">{questions[currentQuestion].questionText}</div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
