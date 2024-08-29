89297fccf5a5125550e0fa842e6aae10c16315dc
mnethaji838 <mnethaji838@gmail.com>
2024-08-29 16:22:56 +0530

    add

---
+++ Projects/React js/Productdetail/app-product/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductPageDesign from './Product';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductPageDesign/>
+    </div>
+  );
+}
+
+export default App;
