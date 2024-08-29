1c895214ee662a0493be5d39701e23fac55906b0
mnethaji838 <mnethaji838@gmail.com>
2024-08-29 14:58:20 +0530

    add

---
+++ Projects/React js/PasswordStrength/app-passwordstrength/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PasswordStrength from './Passwordstrength';
+
+function App() {
+  return (
+    <div className="App">
+     <PasswordStrength/>
+    </div>
+  );
+}
+
+export default App;
