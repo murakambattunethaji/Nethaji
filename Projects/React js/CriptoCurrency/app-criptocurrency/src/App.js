0785d87d14647295558d579da4ab65f2555dc646
mnethaji838 <mnethaji838@gmail.com>
2024-08-29 16:05:03 +0530

    add

---
+++ Projects/React js/CriptoCurrency/app-criptocurrency/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CryptoCurrency from './Cripto';
+
+function App() {
+  return (
+    <div className="App">
+    <CryptoCurrency/>
+    </div>
+  );
+}
+
+export default App;
