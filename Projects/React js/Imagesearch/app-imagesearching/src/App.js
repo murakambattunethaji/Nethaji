827817c631d0fb4b0e5a7b545fdc66d6f47a6b3c
mnethaji838 <mnethaji838@gmail.com>
2024-08-30 11:40:53 +0530

    add

---
+++ Projects/React js/Imagesearch/app-imagesearching/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageSearchEngine from './Imagesearching';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSearchEngine/>
+    </div>
+  );
+}
+
+export default App;
