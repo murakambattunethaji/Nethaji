ad0dc0028ffb50e29a7c10ea267d72d8f043b830
mnethaji838 <mnethaji838@gmail.com>
2024-10-11 12:35:57 +0530

    add

--- Projects/React js/app-evalution/src/App.js
+++ Projects/React js/app-evalution/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import video1 from "../src/assets/video1.mp4";
+import image1 from "../src/assets/image1.png";
+import image2 from "../src/assets/image2.png";
+import image3 from "../src/assets/image3.png";
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Background = ({playStatus,heroCount}) => {
+
+    if (playStatus) {
+        return (
+            <video className="background fade-in" autoPlay loop muted>
+                <source src={video1} type="video/mp4"/>
+            </video>
+        )
+    }
+    else if(heroCount===0) {
+        return <img src={image1} className="background fade-in" alt="" />
+    }
+    else if(heroCount===1) {
+        return <img src={image2} className="background fade-in" alt="" />
+    }
+    else if(heroCount===2) {
+        return <img src={image3} className="background fade-in" alt="" />
+    }
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default App
+export default Background;
