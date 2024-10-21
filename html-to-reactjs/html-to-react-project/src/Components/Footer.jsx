87cb5d37a93fd7e748f1c1901e1c73f6af1ece6f
mnethaji838 <mnethaji838@gmail.com>
2024-10-21 20:38:12 +0530

    html-to-classcomponent

---
+++ html-to-reactjs/html-to-react-project/src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+
+export class Footer extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <h1>Calender</h1>
+                <div class="hero">
+                    <div class="calender">
+                        <div class="left">
+                            <p id="date">01</p>
+                            <p id="day">Sunday</p>
+                        </div>
+                        <div class="right">
+                            <p id="month">January</p>
+                            <p id="year">2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+} 
---
+++ html-to-reactjs/html-to-react-project/src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+export class Header extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <h1>Digital Clock</h1>
+                <div class="hello">
+                    <div class="container">
+                        <div class="clock">
+                            <span id="hrs">00</span>
+                            <span>:</span>
+                            <span id="min">00</span>
+                            <span>:</span>
+                            <span id="sec">00</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
