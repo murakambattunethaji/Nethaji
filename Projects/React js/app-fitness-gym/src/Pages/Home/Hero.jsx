177b776420503ee9da2ac8517a8c1cac35a414a3
mnethaji838 <mnethaji838@gmail.com>
2024-10-21 15:02:10 +0530

    add

--- Projects/React js/app-fitness-gym/src/Pages/Home/Hero.jsx
+++ Projects/React js/app-fitness-gym/src/Pages/Home/Hero.jsx
@@ -85,7 +85,32 @@ const Hero = () => {
                         </h5>
                         <p className="text-lg text-gray-600 font-medium">Members</p>
                     </div>
+                    <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48%]
+                    sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10 hover: bg-black/30
+                    rounded-md ease-out duration-700 cursor-pointer">
+                        <h5 className="text-4x1 text-indigo-400 font-semibold mb-2 uppercase">
+                            120+
+                        </h5>
+                        <p className="text-lg text-gray-600 font-medium">Export Trainers</p>
+                    </div>
+                    <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48%]
+                    sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10 hover: bg-black/30
+                    rounded-md ease-out duration-700 cursor-pointer">
+                        <h5 className="text-4x1 text-indigo-400 font-semibold mb-2 uppercase">
+                            13+
+                        </h5>
+                        <p className="text-lg text-gray-600 font-medium">years of experience</p>
+                    </div>
+                    <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48%]
+                    sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10 hover: bg-black/30
+                    rounded-md ease-out duration-700 cursor-pointer">
+                        <h5 className="text-4x1 text-indigo-400 font-semibold mb-2 uppercase">
+                            368+
+                        </h5>
+                        <p className="text-lg text-gray-600 font-medium">GYMs</p>
+                    </div>
                 </div>
+                
             </div>
         </>
     );
