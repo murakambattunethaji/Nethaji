8d0bcfa2bf6a85c09255d38f33d466f1b2549114
mnethaji838 <mnethaji838@gmail.com>
2024-10-23 10:28:14 +0530

    online-education-start-project

--- Online_Education/my-vite-app/src/App.jsx
+++ Online_Education/my-vite-app/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App (){
   return (
     <>
-      <div className="w-full min-h-screen flex flex-col bg-neutral-50">
+      <div className="w-full min-h-screen flex flex-col bg-neutral  `-50">
         hello there !!!
       </div>
     </>
