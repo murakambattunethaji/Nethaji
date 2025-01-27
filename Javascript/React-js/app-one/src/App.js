ecf896c0c051d4020ab2f4794990d93463ea5411
mnethaji838 <mnethaji838@gmail.com>
2025-01-27 13:34:37 +0530

    added

--- Javascript/React-js/app-one/src/App.js
+++ Javascript/React-js/app-one/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Sample1 from './Sample1';
+import Sample1 from './Components/Sample1';
+import Sample7 from './Components/Sample7';
 
 function App() {
   return (
     <div className="App">
-      <Sample1/>
+      {/* <Sample1/> */}
+      <Sample7/>
     </div>
   );
 }
