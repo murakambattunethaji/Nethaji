000c6a31b2780407bdcf15602922d0f34b0bad9d
mnethaji838 <mnethaji838@gmail.com>
2025-01-28 09:50:17 +0530

    add

---
+++ Javascript/React-js/app-two/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Sample1 from './Components/Sample1';
+
+function App() {
+  return (
+    <div className="App">
+     <Sample1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Javascript/React-js/app-two/src/Redux/sample1.js
@@ -0,0 +1,7 @@
+import {createSlice } from "@reduxjs/toolkit "
+
+const cartmanagement = createSlice({
+    name:'cartmanagement',
+    initialState:[],
+    reducers:{},
+})
